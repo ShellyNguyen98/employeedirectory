7b5452a6b6967bc1e399917a84b836d6a4ef3d88
Trang Nguyen <ShellyNguyen98@gmail.com>
2020-09-18 23:10:00 -0700

    update

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from 'react'
+import 'react-table-6/react-table.css'
+import axios from 'axios'
+import ReactTable from 'react-table-6'
+
+
+const App = () =>  {
+
+  const [employeeState, setEmployeeState] = useState ({
+    employees: [],
+    comlums: [
+      {
+        Header: 'Name',
+        accessor: 'name',
+      },
+      {
+        Header: 'Email',
+        accessor: 'email',
+      },
+      {
+        Header: 'Phone',
+        accessor: 'phone',
+      },
+      {
+        Header: 'Country',
+        accessor: 'Country',
+      },
+    ]
+  })
+
+  useEffect (() => {
+    axios.get('https://randomuser.me/api?results=20')
+    .then (({ data }) => {
+      console.log(data.results)
+
+  let employees = data.results.map(employee => ({
+    name: employee.name.first+''+employee.name.last,
+    email: employee.email,
+    phone: employee.phone,
+    country: employee.location.country
+  }))
+  
+
+  setEmployeeState({ ...employeeState, employees})
+    })
+    .catch (err => console.log(err))
+  // eslint-disable-next-line
+  }, [])
+  return (
+    <ReactTable
+      data= {employeeState.employees}
+      comlums={employeeState.columes}
+    />
+  )
+  
+}
+
+export default App;
